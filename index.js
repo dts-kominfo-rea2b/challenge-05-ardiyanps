883ef5c6651dabc1909c628b394f46445325e1f4
Ardiyan puji santoso <40789455+ardiyanps@users.noreply.github.com>
2022-06-04 12:22:13 +0700

    bismillah merchandise :)

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (name, sorting) => {
+  arrayName = [];
+
+  if (sorting === undefined) {
+    let y = name.sort();
+    for (let i = 0; i < y.length; i++) {
+      const a = y[i];
+      const index = (`${i+1}. ${a}`);
+      arrayName.push(index);
+    }
+  } else {
+    let x = sorting(name);
+    for (let i = 0; i < x.length; i++) {
+      const a = x[i];
+      const index = (a);
+      arrayName.push(index);
+    }
+  }
+  return arrayName;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (name) => {
+  asc = name.sort();
+  return asc;
+} 
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (name) => {
+  x = name.sort();
+  des = x.reverse()
+  return des;
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
